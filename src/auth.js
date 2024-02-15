ea1b8b271364f0c5c24c6e53257a646f96851307
irungundigirigi <irungundigirigi@gmail.com>
2024-02-15 21:34:53 +0300

    update server

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 
 export const { handlers:{ GET, POST }, auth, signIn, signOut } = NextAuth({ providers: [GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientId:"339642451463-60aql446a4naignl2l49k71c1bvlkfh8.apps.googleusercontent.com",
+    clientSecret:"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",
 
 })]})
