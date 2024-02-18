26babf7262abee251c84267b3f7bd614eb5a5074
irungundigirigi <irungundigirigi@gmail.com>
2024-02-18 12:02:34 +0300

    update

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,20 +3,21 @@ import styles from './Hero.module.css';
 
 const CoverComponent = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.heroContainer}>
       <div className={styles.imageContainer}>
         <Image
           src="/cover.png"
           alt="Mt Kenya"
           className={styles.img}
-          layout="responsive"
           width='300'
           height='300'
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Curated Mt Kenya Hiking experiences</h1>
-        <h2 className={styles.subtitle}>Share your experiences</h2>
+        <div className={styles.heroText}>
+          <h1 className={styles.title}>Curated Mt Kenya Hiking experiences</h1>
+          <h2 className={styles.subtitle}>Share your experiences</h2>
+        </div>
       </div>
     </div>
   );
